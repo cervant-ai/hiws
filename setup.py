a2ae680335e65039b8f5bd003469579862ac123f
Tomas Santana <tomas.30604530@uru.edu>
2025-09-22 23:19:06 -0400

    chore: bump version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="hiws",
-    version="0.1.1",
+    version="0.1.2",
     author="Tomas Santana",
     author_email="tomas@cervant.chat",
     description="A simple Python wrapper for WhatsApp's Cloud API",
