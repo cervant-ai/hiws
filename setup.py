53aed189e18f4c44def0dfa4f293139b6f9cba77
Tomas Santana <tomas.30604530@uru.edu>
2025-09-23 01:09:41 -0400

    chore: bump version number to 0.2.1 and update required Python version to 3.8

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="hiws",
-    version="0.2.0",
+    version="0.2.1",
     author="Tomas Santana",
     author_email="tomas@cervant.chat",
     description="A simple Python wrapper for WhatsApp's Cloud API",
